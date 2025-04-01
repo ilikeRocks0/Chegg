bb03c26b51a1b9547d79811df99ce9901f8f1106
Ty Paragas <ty.paragas01@gmail.com>
2025-03-31 21:58:30 -0500

    Update ActiveBoardPiece

--- ActiveBoardPiece.py
+++ ActiveBoardPiece.py
@@ -4,6 +4,6 @@ class ActiveBoardPiece(UnitData):
     def __init__(self, playerID:int):
         self.owner = playerID
         self.movedOnce = False
-        self.movedTwice = True
+        self.movedTwice = False
 
-    
+
