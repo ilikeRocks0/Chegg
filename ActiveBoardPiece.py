10eab4c9ed0992b43ebfe964f83fba1d4a4a509d
Ty Paragas <ty.paragas01@gmail.com>
2025-03-31 21:56:24 -0500

    Add ActiveBoardPiece

---
+++ ActiveBoardPiece.py
@@ -0,0 +1,9 @@
+from UnitData import UnitData
+
+class ActiveBoardPiece(UnitData):
+    def __init__(self, playerID:int):
+        self.owner = playerID
+        self.movedOnce = False
+        self.movedTwice = True
+
+    
