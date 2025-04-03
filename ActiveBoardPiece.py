f68717c19a7c00198e4c8460d8ac5096fd90ebae
Marco Gerra <125428652+ilikeRocks0@users.noreply.github.com>
2025-04-02 19:47:21 -0700

    updated ActiveBoardPiece so I dont have to keep changing it everytime I update UnitData

--- ActiveBoardPiece.py
+++ ActiveBoardPiece.py
@@ -1,16 +1,21 @@
 from UnitData import UnitData
 
 class ActiveBoardPiece(UnitData):
-    def __init__(self, playerID:int, parent:UnitData):
+    def __init__(self, playerID: int, parent: UnitData):
+        # Calls the constructor of UnitData to initialize attributes
+        super().__init__(  
+            parent.name,
+            parent.cost,
+            parent.move,
+            parent.attack,
+            parent.special,
+            parent.death
+        )
+        
         self.owner = playerID
         self.movedOnce = False
         self.movedTwice = False
-        self.name = parent.name
-        self.cost = parent.cost
-        self.move = parent.move
-        self.attack = parent.attack
-        self.death = parent.death
-        self.attack = parent.attack
+        self.attacked = False
 
 
 
